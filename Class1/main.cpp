d18a582bb3f623dd9f11ff29286893fc385d0ba5
luyicheng <thirtytwo0301@gmail.com>
2025-04-09 18:43:08 +0800

    Class1更新

--- Class1/main.cpp
+++ Class1/main.cpp
@@ -2,11 +2,22 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <vector>
 #include <stdexcept>
 #include <cstdlib>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+
+const std::vector<const char*> validationLayers = {
+	"VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
 class HelloTriangleApplication
 {
 public:
@@ -26,8 +37,62 @@ private:
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 	}
+	//挑战，检测当前需要的extension是否支持
+	bool CheckExtensionsSupport(const char** extension, uint32_t count)
+	{
+		uint32_t supportExtensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &supportExtensionCount, nullptr);
+		std::vector<VkExtensionProperties> supportExtensions(supportExtensionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &supportExtensionCount, supportExtensions.data());
+	
+		for (int i = 0; i < count; i++)
+		{
+			bool extensionFound = false;
+			for (const auto& supportExtension : supportExtensions)
+			{
+				if (strcmp(extension[i], supportExtension.extensionName) == 0)
+				{
+					extensionFound = true;
+					break;
+				}
+			}
+			if (!extensionFound) return false;
+		}
+		return true;
+	}
+	bool CheckValidationLayerSupport()
+	{
+		uint32_t layerCount;
+		vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+		std::vector<VkLayerProperties> availableLayers(layerCount);
+		vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+		for (const char* layerName : validationLayers)
+		{
+			bool layerFound = false;
+			for (const auto& layerProperties : availableLayers)
+			{
+				if (strcmp(layerName, layerProperties.layerName) == 0)
+				{
+					layerFound = true;
+					break;
+				}
+			}
+			if (!layerFound)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
 	void CreateInstance()
 	{
+		if (enableValidationLayers && !CheckValidationLayerSupport())
+		{
+			throw std::runtime_error("validation layers requested, but not available!");
+		}
 		VkApplicationInfo appInfo{};
 		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 		appInfo.pApplicationName = "Hello Triangle";
@@ -39,6 +104,34 @@ private:
 		VkInstanceCreateInfo createInfo{};
 		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 		createInfo.pApplicationInfo = &appInfo;
+
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+
+		//只知道这里是获取glfw对应的extension，让vulkan知道我们用的是这个system
+		//至于extension的具体作用和具体的意义目前不知道
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		if (!CheckExtensionsSupport(glfwExtensions, glfwExtensionCount))
+		{
+			throw std::runtime_error("failed to load extensions");
+		}
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+		if (enableValidationLayers)
+		{
+			createInfo.enabledExtensionCount = static_cast<uint32_t>(validationLayers.size());
+			createInfo.ppEnabledLayerNames = validationLayers.data();
+		}
+		createInfo.enabledLayerCount = 0;
+
+		//这里VkResult 就只是是否Create成功的属性
+		//VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create instance!");
+		}
 	}
 	void InitVulkan()
 	{
@@ -53,13 +146,15 @@ private:
 	}
 	void Cleanup()
 	{
+		vkDestroyInstance(instance, nullptr);
+
 		glfwDestroyWindow(window);
 		
 		glfwTerminate();
 	}
 
 	GLFWwindow* window;
-	VkInstance intance;
+	VkInstance instance;
 };
 
 int main()
