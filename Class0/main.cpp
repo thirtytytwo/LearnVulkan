73803dd10a7fb167d3e1fa8474c045c732c586a4
thirtytwo <58972099+thirtytytwo@users.noreply.github.com>
2025-04-09 09:22:52 +0800

    Class1 Init

--- Class0/main.cpp
+++ Class0/main.cpp
@@ -14,15 +14,6 @@ int main()
 
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	GLFWwindow * window = glfwCreateWindow(800, 600, "Vulkan window",nullptr, nullptr);
-
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-	std::cout << extensionCount << " extensions supported\n";
-
-	glm::mat4 matrix;
-	glm::vec4 vec;
-	auto test = matrix * vec;
 	
 	while (!glfwWindowShouldClose(window))
 	{
